45280f71d1b9caa53c6a278bb6d28bc6d3849498
Tiffany-Dby <tiffany.dby@gmail.com>
2024-04-19 23:01:31 +0200

    "burger menu + some adjustments"

---
+++ src/scripts/headerMenu.js
@@ -0,0 +1,11 @@
+const burgerElt = document.querySelector(".burger");
+const navElt = document.querySelector("header nav");
+
+
+document.addEventListener("astro:page-load", () => {
+  burgerElt.addEventListener("click", () => {
+    burgerElt.classList.toggle("burger--open");
+
+    navElt.classList.toggle("nav--open");
+  });
+});
