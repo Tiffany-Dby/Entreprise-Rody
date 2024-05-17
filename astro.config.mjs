359255372b45b26b945b9dfda2708613018d6dc1
Tiffany-Dby <tiffany.dby@gmail.com>
2024-05-17 22:43:08 +0200

    "added base for Github page deployment"

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  site: 'https://tiffany-dby.github.io'
+  site: 'https://tiffany-dby.github.io',
+  base: 'entreprise-rody'
 });
